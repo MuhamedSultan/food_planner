f67500790c9473ffa6e75a257ffb0290df4dd795
Muhamed Sultan <muhamedsultan211@gmail.com>
2024-08-13 20:06:19 +0300

    Create Home Fragment

--- app/src/main/java/com/example/foodplanner/HomeFragment.java
+++ app/src/main/java/com/example/foodplanner/HomeFragment.java
@@ -1,4 +1,4 @@
-package com.example.foodplanner.auth;
+package com.example.foodplanner;
 
 import android.os.Bundle;
 
@@ -8,14 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.foodplanner.R;
-
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link AuthFragment#newInstance} factory method to
+ * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AuthFragment extends Fragment {
+public class HomeFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +24,7 @@ public class AuthFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public AuthFragment() {
+    public HomeFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +34,11 @@ public class AuthFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment AuthFragment.
+     * @return A new instance of fragment HomeFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AuthFragment newInstance(String param1, String param2) {
-        AuthFragment fragment = new AuthFragment();
+    public static HomeFragment newInstance(String param1, String param2) {
+        HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +59,6 @@ public class AuthFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_auth, container, false);
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
