092d4c7a7b33fedd13de89fa2df9e4b46c73e384
Muhamed Sultan <muhamedsultan211@gmail.com>
2024-08-14 00:18:44 +0300

    Check If User Login

--- app/src/main/java/com/example/foodplanner/MainActivity.java
+++ app/src/main/java/com/example/foodplanner/MainActivity.java
@@ -7,8 +7,12 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
     NavController navController;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +21,23 @@ public class MainActivity extends AppCompatActivity {
 
         navController = Navigation.findNavController(this, R.id.navHostfragment);
         NavigationUI.setupActionBarWithNavController(this, navController);
+        firebaseAuth = FirebaseAuth.getInstance();
 
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         return navController.navigateUp() || super.onSupportNavigateUp();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser=firebaseAuth.getCurrentUser();
+        if (currentUser!=null){
+            navController.navigate(R.id.homeFragment);
+        }else {
+            navController.navigate(R.id.authFragment);
+        }
     }
 }
