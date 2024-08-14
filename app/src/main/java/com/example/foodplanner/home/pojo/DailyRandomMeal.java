a3260012abbf5b27f3d123b6353c77a7e4e370cb
Muhamed Sultan <muhamedsultan211@gmail.com>
2024-08-14 20:33:53 +0300

    Create Meal Response

---
+++ app/src/main/java/com/example/foodplanner/home/pojo/DailyRandomMeal.java
@@ -0,0 +1,19 @@
+// DailyRandomMeal.java
+
+// YApi QuickType插件生成，具体参考文档:https://plugins.jetbrains.com/plugin/18847-yapi-quicktype/documentation
+
+package com.example.foodplanner.home.pojo;
+
+import java.util.List;
+
+public class DailyRandomMeal {
+    private List<Meal> meals;
+
+    public List<Meal> getMeals() {
+        return meals;
+    }
+
+    public void setMeals(List<Meal> value) {
+        this.meals = value;
+    }
+}
