a1c3a79b87d62210197a58dddb73088ee71e4615
Muhamed Sultan <muhamedsultan211@gmail.com>
2024-08-14 20:27:41 +0300

    Add Rx Java And Room Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -45,17 +44,46 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
-    implementation ("androidx.navigation:navigation-fragment:2.5.3")
-    implementation ("androidx.navigation:navigation-ui:2.5.3")
-    implementation ("de.hdodenhof:circleimageview:3.1.0")
-    implementation ("com.airbnb.android:lottie:6.0.0")
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment:2.5.3")
+    implementation("androidx.navigation:navigation-ui:2.5.3")
 
+    // Circle Image
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+
+    // Lottie Animation
+    implementation("com.airbnb.android:lottie:6.0.0")
+
+    // Firebase
     implementation("com.google.firebase:firebase-bom:33.1.2")
-    implementation ("com.google.firebase:firebase-auth:22.0.0")
-    implementation ("com.google.android.gms:play-services-auth:20.7.0")
+    implementation("com.google.firebase:firebase-auth:22.0.0")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+
+    // Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.7.2")
+    implementation ("com.squareup.retrofit2:converter-gson:2.7.2")
+    implementation ("com.squareup.okhttp3:okhttp:3.6.0")
+
+    // Gson
+    implementation("com.google.code.gson:gson:2.11.0")
+
+    // OkHttp
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:3.9.1")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    // Room
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+
+    implementation ("com.github.akarnokd:rxjava3-retrofit-adapter:3.0.0")
+    implementation ("io.reactivex.rxjava3:rxandroid:3.0.2")
+    implementation ("io.reactivex.rxjava3:rxjava:3.1.6")
+    implementation("com.squareup.retrofit2:adapter-rxjava3:2.11.0")
 
 
-
-
-
+    // Room with RxJava support
+    implementation ("androidx.room:room-rxjava3:2.4.1")
 }
