9e18ab0ebbd72235fbf60c255c266d8455fb4f20
Muhamed Sultan <muhamedsultan211@gmail.com>
2024-08-14 20:29:44 +0300

    Create Api Service

---
+++ app/src/main/java/com/example/foodplanner/api/ApiService.java
@@ -0,0 +1,11 @@
+package com.example.foodplanner.api;
+
+import com.example.foodplanner.home.pojo.DailyRandomMeal;
+
+import io.reactivex.rxjava3.core.Single;
+import retrofit2.http.GET;
+
+public interface ApiService {
+    @GET("api/json/v1/1/random.php")
+    Single<DailyRandomMeal> getDailyMeal();
+}
