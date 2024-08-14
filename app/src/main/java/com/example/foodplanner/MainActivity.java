fd98c6a88438cf775dd6f37496443ceb1d0b8e32
Muhamed Sultan <muhamedsultan211@gmail.com>
2024-08-14 13:23:07 +0300

    Apply Bottom Navigation View

--- app/src/main/java/com/example/foodplanner/MainActivity.java
+++ app/src/main/java/com/example/foodplanner/MainActivity.java
@@ -7,22 +7,25 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.example.foodplanner.databinding.ActivityMainBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    public ActivityMainBinding binding;
     NavController navController;
     FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         navController = Navigation.findNavController(this, R.id.navHostfragment);
         NavigationUI.setupActionBarWithNavController(this, navController);
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
         firebaseAuth = FirebaseAuth.getInstance();
-
     }
 
     @Override
